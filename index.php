73fcf47dc104a03f854b73723be4d8ac780e338e
hanko-van <90352830+niki-van13@users.noreply.github.com>
2022-01-12 22:03:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Naturreservatet<h1>
+
+    <form action= "./results.php" method = "POST">
+    <input placeholder= "apa" type="text" name="apa">
+    <input placeholder= "giraff" type="text" name="giraff">
+    <input placeholder= "tigrar" type="text" name="tigrar">
+    <input placeholder= "kokosnötter" type="text" name="kokosnötter">
+    
+<button type="submit">Save</button>
+
+</form>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
